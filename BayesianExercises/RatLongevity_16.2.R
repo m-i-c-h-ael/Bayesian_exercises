1ab850602f525257f97990f06d7c5b18bd8ce147
m-i-c-h-ael <plank.michael.j@gmail.com>
2023-08-10 08:26:26 +0200

    Ran model on 16.2

--- BayesianExercises/RatLongevity_16.2.R
+++ BayesianExercises/RatLongevity_16.2.R
@@ -4,10 +4,17 @@
 #Two-group analysis describing the two groups with student t-distributions with
  #individual shape parameters, but common normality parameter
 
+rm(list=ls())
+cat('\014')
+graphics.off()
+
+library('rstan')
+
+#specify model
 stanCode= '
-data{
+data {
   int<lower=1> Ntotal;       //number of rats
-  real<lower=0> y[Ntotal]:  //rat lifespan
+  real<lower=0> y[Ntotal];  //rat lifespan
   int x[Ntotal];            //category: ad libidum or CR
   real<lower=0> y_mean;
   real<lower=0> y_sd;
@@ -40,4 +47,27 @@ model{
   }
 }
 '
+# prepare data
+data= read.csv('H:/My Drive/learning/20230220_BayesianStatistics/20230504_BayesianDogsBook/DBDA2Eprograms/RatLives.csv')
+head(data)
+dataList= list(
+  x= as.numeric(as.factor(data$Group)),
+  y= data$DaysLive,
+  Ntotal= dim(data)[1],
+  y_mean= mean(data$DaysLive),
+  y_sd= sd(data$DaysLive)
+)
 
+#run MCMC
+DSO= stan_model(model_code= stanCode)
+stanOUT= sampling(DSO, data=dataList, chains=3, iter=1000, warmup=500)
+
+# isolate chains
+chain1= as.array(stanOUT)[,1,]
+chain2= as.array(stanOUT)[,2,]
+chain3= as.array(stanOUT)[,3,]
+head(chain1)
+
+#check runs
+plot(chain1[,2],main='mu1',type='l')
+lines(chain2[,2],col='red')
